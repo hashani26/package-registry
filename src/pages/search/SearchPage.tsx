695acc66e976b6a3f83353427ed80b3fe1110e2b
Hashani26 <hashanilakshi@gmail.com>
2025-02-03 19:34:00 +0530

    finalise search page

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,11 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import type { SearchLoaderResult } from "./SearchLoader";
+import PackageListItem from "../../components/PackageListItem";
 
 function SearchPage() {
   const data = useLoaderData() as SearchLoaderResult; //why not assert on data?
   console.log("🚀 ~ SearchPage ~ data:", data);
 
-  return <div>Search Page</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold my-6">Search Results</h1>
+      <div className="space-y-4 mt-4">
+        {data.searchResults.map((pack) => (
+          <PackageListItem key={pack.name} pack={pack} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
